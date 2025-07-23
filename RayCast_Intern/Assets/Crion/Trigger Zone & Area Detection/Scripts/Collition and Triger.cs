c86e0044e24a7b6d5cd82329c5e803b9680345be
Hemnathcrion <hemnathcrion@gmail.com>
2025-07-23 13:16:39 +0530

    Fire ParticalSystem Completed

--- RayCast_Intern/Assets/Crion/Trigger Zone & Area Detection/Scripts/Collition and Triger.cs
+++ RayCast_Intern/Assets/Crion/Trigger Zone & Area Detection/Scripts/Collition and Triger.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class CollitionandTriger : MonoBehaviour
 {
     public Light[] target;
+    public Light[] topLight;
+    public GameObject particleSystems;
+    public Light fireLight;
     private void Start()
     {
         foreach(Light light in target)
         {
             light.enabled = false;
         }
+        foreach(Light light1 in topLight) 
+            {
+                light1.enabled = false;
+            }
+      particleSystems.SetActive(false);
+        fireLight.enabled = false;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -24,7 +33,36 @@ public class CollitionandTriger : MonoBehaviour
             }
 
         }
+        if( other.CompareTag("TopLight"))
+        {
+            foreach(Light light1 in topLight)
+            {
+                light1.enabled =true;
+                Debug.Log("Enterd");
+            }
+
+        }
+       
 
 
+
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Fire"))
+        {
+            particleSystems.SetActive(true);
+            fireLight.enabled = true;
+
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Fire"))
+        {
+            particleSystems.SetActive(false);
+            fireLight.enabled = false;
+
+        }
     }
 }
